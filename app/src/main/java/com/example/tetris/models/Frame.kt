c381c70efe7e37231b29c20a66c004b72ef7bae6
Zabalkanskiy <azabalkanskij@hotmail.com>
2019-11-26 21:40:09 +0300

    add helper function

---
+++ app/src/main/java/com/example/tetris/models/Frame.kt
@@ -0,0 +1,22 @@
+package com.example.tetris.models
+
+import com.example.tetris.helpers.array2dOfByte
+
+class Frame(private val  with:Int){
+    val data:ArrayList<ByteArray> = ArrayList()
+
+    fun addRow(byteStr:String):Frame{
+        val row = ByteArray(byteStr.length)
+
+        for (index in byteStr.indices){
+            row[index] = "${byteStr[index]}".toByte()
+        }
+        data.add(row)
+        return this
+    }
+
+    fun as2dToByteArray():Array<ByteArray>{
+        val bytes = array2dOfByte(data.size,with)
+        return data.toArray(bytes)
+    }
+}
