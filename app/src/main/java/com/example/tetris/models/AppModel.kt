93bb728e2920d1d90946c1be68ed45553e02ac5d
Zabalkanskiy <azabalkanskij@hotmail.com>
2019-12-01 21:30:41 +0300

    Add logic in AppModel and create CellCostants

--- app/src/main/java/com/example/tetris/models/AppModel.kt
+++ app/src/main/java/com/example/tetris/models/AppModel.kt
@@ -4,6 +4,8 @@ import android.graphics.Point
 import com.example.tetris.models.FieldConstants.FieldConstants
 import com.example.tetris.helpers.array2dOfByte
 import com.example.tetris.storage.AppPreference
+import com.example.tetris.constants.CellConstants
+import java.text.FieldPosition
 
 class AppModel {
 
@@ -40,5 +42,72 @@ class AppModel {
             field[row][column] = status
         }
 
+    }
+
+    fun isGameOver():Boolean {
+        return currentState == Statuses.OVER.name
+
+    }
+
+    fun isGameActive():Boolean{
+        return currentState == Statuses.ACTIVE.name
+    }
+
+    fun isGameAwaitingStart():Boolean{
+        return currentState == Statuses.AWAITING_START.name
+    }
+
+    private fun boostScore(){
+        score+=10
+        if(score>preference?.getHighScore()as Int){
+            preference?.saveHighScore(score)
+        }
+    }
+
+    private fun generateNextBlock(){
+        currentBlock = Block.createBlock()
+    }
+
+    private fun validTranslation(position: Point, shape: Array<ByteArray>):Boolean{
+         if(position.x<0 || position.y<0){
+             return  false
+        } else if(position.y + shape.size>FieldConstants.ROW_COUNT.value){
+             return false
+
+         }else if(position.x + shape[0].size> FieldConstants.COLUMN_COUNT.value){
+             return false
+         } else{
+             for ( i in 0 until shape.size){
+                 for (j in 0 until shape[i].size){
+                     val y =position.y +i
+                     val x =position.x +j
+
+                     if(CellConstants.EMPTY.value != shape[i][j] && CellConstants.EMPTY.value != field[x][y]){
+                            return false
+                     }
+                 }
+             }
+
+         }
+        return true
+    }
+
+    private fun moveValid(position: Point, frameNumber: Int?):Boolean{
+        val shape:Array<ByteArray>? = currentBlock?.getShape(frameNumber as Int)
+        return validTranslation(position, shape as Array<ByteArray>)
+    }
+
+    fun generateField(action: String){
+        if(isGameActive()){
+            resetField()
+            var frameNumber = currentBlock?.frameNumber
+            var coordinate: Point? = Point()
+            coordinate?.x = currentBlock?.position?.x
+            coordinate?.y = currentBlock?.position?.y
+
+            when(action){
+                
+            }
+        }
     }
 }
